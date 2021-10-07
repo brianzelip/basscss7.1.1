6bdc60558e58670be3edc979eec58e0f86a52c82
Brian Zelip <bzelip@protonmail.com>
2021-10-07 15:23:56 -0400

    Lint test.js, reorder imports

--- test.js
+++ test.js
@@ -1,33 +1,33 @@
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
-const assert = require('assert/strict');
+const path = require('path')
+const fs = require('fs')
+const https = require('https')
+const assert = require('assert/strict')
 
-const localFileName = 'basscss@7.1.1.min.css';
-const localPath = path.join(__dirname, localFileName);
-const localBasscss = fs.readFileSync(localPath, { encoding: 'utf-8' });
+const sourceUrl = 'https://unpkg.com/basscss@7.1.1/css/basscss.min.css'
 
-const sourceUrl = 'https://unpkg.com/basscss@7.1.1/css/basscss.min.css';
+const localFileName = 'basscss@7.1.1.min.css'
+const localPath = path.join(__dirname, localFileName)
+const localBasscss = fs.readFileSync(localPath, { encoding: 'utf-8' })
 
 https
-  .get(sourceUrl, (res) => {
-    let sourceBasscss = '';
+  .get(sourceUrl, res => {
+    let sourceBasscss = ''
 
-    res.on('data', (chunk) => {
-      sourceBasscss += chunk;
-    });
+    res.on('data', chunk => {
+      sourceBasscss += chunk
+    })
 
     res.on('end', () => {
       try {
-        assert.strictEqual(localBasscss.trim(), sourceBasscss.trim());
+        assert.strictEqual(localBasscss.trim(), sourceBasscss.trim())
         console.log(
           '✅ Test passed! The local Basscss@7.1.1 file is equal to the source Basscss@7.1.1 file.'
-        );
+        )
       } catch (e) {
-        console.error(e.message);
+        console.error(e.message)
       }
-    });
+    })
   })
-  .on('error', (e) => {
-    console.error(e);
-  });
+  .on('error', e => {
+    console.error(e)
+  })
